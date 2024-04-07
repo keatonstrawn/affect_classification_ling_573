e7d2aaf92f1bcfdb86a4ede86107c4c68dd31f9d
skinnel <lindsaypskinner@gmail.com>
2024-04-06 20:39:02 -0700

    Set up framework for feature engineering.

---
+++ src/feature_engineering.py
@@ -0,0 +1,127 @@
+""" This script defines a helper class to take the cleand HatEval data and generate features that a classification model
+can use to classify the tweets within the dataset.
+"""
+
+# Libraries
+import pandas as pd
+
+from typing import Optional
+
+# Define class to perform feature engineering
+class FeatureEngineering:
+
+    def __init__(self):
+        """Generates features from processed data to be used in hate speech detection tasks A and B, as specified in SemEval 2019
+        task 5.
+
+        Includes methods to generate the following features:
+            * example feature1 --> fill this in with actual feature
+            * example feature2 --> fill this in with actual feature
+            * example feature3 --> fill this in with actual feature
+
+            Attributes
+            ----------
+            train_data
+                The cleaned training data that results from the data processing steps.
+        """
+
+        # Initialize the cleaned datasets
+        self.train_data: Optional[pd.DataFrame] = None
+
+        # Set fit flag
+        self.fitted = False
+
+    def _example_feature1_method(self, data: pd.DataFrame, fit: bool, other_args):
+        """This is a placeholder for a method that would be implemented to generate a particular feature from the data.
+
+        Arguments:
+        ---------
+        data
+            The data for which the feature is to be generated
+        fit
+            If true then we assume the provided data is training data and the method is allowed to learn any necessary
+            values from the dataset. If false then we assume the provided data is validation or test data and the
+            values necessary to generate the feature have been pre-computed from the training data in a previous step.
+        other_args
+            Include any additional arguments that the method may require.
+
+        Returns:
+        -------
+        The original dataset with a new column that contains the new feature generated for each observation in the
+        dataset.
+        """
+
+        return data
+
+    def fit_transform(self, train_data):
+        """Learns all necessary information from the provided training data in order to generate the complete set of
+        features to be fed into the classification model. In the fitting process, the training data is also transformed
+        into the feature-set expected by the model and returned.
+
+        Arguments:
+        ---------
+        train_data
+            The training data that is used to define the feature-engineering methods.
+
+        Returns:
+        -------
+        transformed_data
+            The original train_data dataframe with new columns that include the calculated features for each observation
+            in the dataset.
+        """
+
+        # Get the training data, to be used for fitting
+        self.train_data = train_data
+
+        # Add in steps for each feature that is to be generated
+        transformed_data = self._example_feature1_method(train_data, fit=True, other_args=None)
+
+        # Update the fitted flag
+        self.fitted = True
+
+        return transformed_data
+
+    def transform(self, data):
+        """Uses the feature-generating methods that were fit in an earlier step to transform a new dataset to include
+        the feature-set expected by the classification model.
+
+        Arguments:
+        ---------
+        data
+            The data set for which the feature set is to be generated.
+
+        Returns:
+        -------
+        transformed_data
+            The original dataframe with new columns that include the calculated features for each observation in the
+            dataset.
+        """
+
+        # Ensure feature generating methods have been trained prior to transforming the data
+        assert self.fitted, 'Must apply fit_transform to training data before other datasets can be transformed.'
+
+        # Add in steps for each feature that is to be generated
+        transformed_data = self._example_feature1_method(data, fit=False, other_args=None)
+
+        return transformed_data
+
+
+if __name__ == '__main__':
+
+    # Imports
+    from src.data_processor import DataProcessor
+
+    # Load and clean the raw data
+    myDP = DataProcessor()
+    myDP.load_data(language='english', filepath='/put/data/directory/filepath/here')
+    myDP.clean_data()
+
+    # Instantiate the FeatureEngineering object
+    myFE = FeatureEngineering()
+
+    # Fit
+    train_df = myFE.fit_transform(myDP.processed_data['train'])
+
+    # Transform
+    val_df = myFE.transform(myDP.processed_data['validation'])
+
