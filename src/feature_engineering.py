2fd2a9b1f7e167100fa442323167847f50374043
Ben C <bencote2000@gmail.com>
2024-04-09 16:04:39 -0700

    Updated number of columns added to reflect NRC dimensions, removed unnecessary fit variable from NRC method

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -16,7 +16,7 @@ class FeatureEngineering:
         SemEval 2019 task 5.
 
         Includes methods to generate the following features:
-            * _NRC_counts --> binary classification of words across eight emotional dimensions. Raw counts are then
+            * _NRC_counts --> binary classification of words across ten emotional dimensions. Raw counts are then
                                     transformed into proportions to normalize across tweets.
             * example feature2 --> fill this in with actual feature
             * example feature3 --> fill this in with actual feature
@@ -28,10 +28,10 @@ class FeatureEngineering:
         # Set fit flag
         self.fitted = False
 
-    def _NRC_counts(self, data: pd.DataFrame, fit: bool):
+    def _NRC_counts(self, data: pd.DataFrame):
         """This method uses data from the NRC Word-Emotion Association Lexicon, which labels words with either a 1 or 0 based on
         the presence or absence of each of the following emotional dimensions: anger, anticipation, disgust, fear, joy, negative, 
-        positive, sadness, surprise, trust. It sums the frequency counts in each of the eight dimensions across all the words 
+        positive, sadness, surprise, trust. It sums the frequency counts in each of the ten dimensions across all the words 
         in a tweet, then divides by the total number of counts to obtain a proportion. These proportions are added on to the end 
         of the dataframe as count-based features.
 
@@ -40,25 +40,21 @@ class FeatureEngineering:
         ---------
         data
             The data for which the feature is to be generated
-        fit
-            If true then we assume the provided data is training data and the method is allowed to learn any necessary
-            values from the dataset. If false then we assume the provided data is validation or test data and the
-            values necessary to generate the feature have been pre-computed from the training data in a previous step.
 
         Returns:
         -------
-        The original dataset with eight new columns that contain the new emotion features generated for each tweet in the
+        The original dataset with ten new columns that contain the new emotion features generated for each tweet in the
         dataset.
 
         """
-        # add eight columns to the end of the dataframe, representing the eight emotional dimensions of NRC
-        emotions = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'negative', 'positive', 'sadness', 'surprise', 'trust']
+        # add ten columns to the end of the dataframe, representing the eight emotional dimensions of NRC
+        emotions = ['negative', 'positive', 'anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust']
         for emotion in emotions:
             data[emotion] = 0
         
         # iterate over each tweet to get counts of each emotion classification
         for index, row in data.iterrows():
-            text = word_tokenize(row['raw_text'])
+            text = word_tokenize(row['cleaned_text'])
 
             # iterate over each word in the tweet, add counts to emotion vector
             for word in text:
@@ -70,9 +66,9 @@ class FeatureEngineering:
 
         # divide by total count of emo markers to get proportions not frequency counts
         # Replace 0 values with NaN to prevent error with dividing by zero
-        rowsums = data.iloc[:, -8:].sum(axis=1)
+        rowsums = data.iloc[:, -10:].sum(axis=1)
         rowsums[rowsums == 0] = float('NaN')
-        data.iloc[:, -8:] = data.iloc[:, -8:].div(rowsums, axis=0)
+        data.iloc[:, -10:] = data.iloc[:, -10:].div(rowsums, axis=0)
 
         # ***Uncomment the line below to create file showing the data visualized***
         # data.to_csv('test.txt', sep=',', header=True)
@@ -101,7 +97,7 @@ class FeatureEngineering:
 
         # Framework to add in steps for each feature that is to be generated
         # transformed_data = self._example_feature1_method(train_data, fit=True, other_args=None)
-        transformed_data = self._NRC_counts(train_data, fit=True)
+        transformed_data = self._NRC_counts(train_data)
 
         # TODO: add in code below to fit and transform training data to generate other features as they are added
 
@@ -130,7 +126,7 @@ class FeatureEngineering:
         assert self.fitted, 'Must apply fit_transform to training data before other datasets can be transformed.'
 
         # Framework to add in steps for each feature that is to be generated
-        transformed_data = self._NRC_counts(data, fit=False)
+        transformed_data = self._NRC_counts(data)
 
         # TODO: add in code below to transform datasets to generate other features as they are added
 
