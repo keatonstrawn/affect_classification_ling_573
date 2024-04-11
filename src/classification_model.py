714b2b60dc379dc3d5be06fd5322b65e2a5e17f6
skinnel <lindsaypskinner@gmail.com>
2024-04-11 11:04:22 -0700

    Added random forest to ClassificationModel class.

--- src/classification_model.py
+++ src/classification_model.py
@@ -4,7 +4,9 @@ the features generated from the FeatureEngineering class."""
 # Libraries
 import pandas as pd
 
+from sklearn.ensemble import RandomForestClassifier
 from typing import List, Optional, Dict
+from copy import deepcopy
 
 
 # Define class to house classification models
@@ -15,8 +17,10 @@ class ClassificationModel:
         SemEval 2019 task 5.
 
         Includes methods to train the following classification models:
-            * baseline model: guesses the target class most commonly seen in the training data
-            * example model2 --> fill this in with model info
+            * baseline model: Guesses the target class most commonly seen in the training data.
+            * random forest: Aggregates probabilistic estimates from decision trees fit on subsets of the dataset to
+                predict the target class. (Note: The probability aggregation is not the same as the approach that allows
+                each tree to 'vote' for a single class.)
             * example model3 --> fill this in with model info
 
             Parameters
@@ -24,22 +28,30 @@ class ClassificationModel:
             model_type
                 Specifies which type of model is used to perform classification. Options include:
                     'baseline'
-                    'model2'
+                    'random_forest'
                     'model3'
         """
 
         # Initialize model parameters
         self.model_type = model_type
         self.model_objectives = []
+        self.tasks: Optional[List[str]] = None
+        self.target_map = {'hate_speech_detection': 'HS', 'target_or_general': 'TR', 'aggression_detection': 'AG'}
 
         # Initialize flag to indicate whether model training has occurred
         self.fitted = False
 
-        # Create attribute to optionally store training data
+        # Create attribute to optionally store training data and predictions
         self.train_data: Optional[pd.DataFrame] = None
+        self.train_est: Optional[pd.DataFrame] = None
 
         # Create attributes for baseline model
         self.most_frequent_category: Optional[Dict[str, int]] = None
+
+        # Create attributes for random forest classification model
+        self.random_forest_classifier: Optional[RandomForestClassifier] = None
+        self.features: Optional[List[str]] = None
+
 
         # TODO: create necessary attributes for other models as they are added
 
@@ -91,7 +103,64 @@ class ClassificationModel:
 
         return pred_df
 
-    def fit(self, train_data: pd.DataFrame, tasks: List[str], keep_training_data: bool = True) -> pd.DataFrame:
+    def _fit_random_forest_model(self, train_data: pd.DataFrame, features: List[str], tasks: List[str]) -> pd.DataFrame:
+        """Trains a random forest model to predict the target category/categories specified in the tasks list.
+
+        Arguments:
+        ----------
+        train_data
+            The data set, with the complete set of engineered features, that is used to train the model.
+        features
+            The set of features to be used in the classification task.
+        tasks
+            The classification task(s) that the model is being trained to predict. If a list of tasks is given then the
+            same model is trained to predict each of those labels simultaneously. To train an individual model for each
+            task, a new model must be instantiated and fit for each label. Task labels may include any of the following:
+            'hate_speech_detection', 'target_or_general', 'aggression_detection'.
+
+
+        Returns:
+        --------
+        A copy of the original dataframe with new columns appended that contain the random forest classifier predictions
+        for the specified training task(s).
+        """
+
+        # Specify which columns contain the target class(es)
+        task_cols = [self.target_map[t] for t in tasks]
+
+        # Limit training data to include only the features and rid of NAs
+        x_train = train_data[features]
+        x_train = x_train.fillna(0)
+
+        # Specify classification objective(s)
+        y_train = train_data[task_cols]
+        if y_train.shape[1] < 2:
+            y_train = y_train.values.flatten()
+
+        # Initialize Random Forest Classifier
+        clf = RandomForestClassifier(n_estimators=400, criterion='entropy', max_depth=None, min_samples_split=0.1,
+                                     min_samples_leaf=3, max_features='sqrt', bootstrap=True, n_jobs=None,
+                                     random_state=42, class_weight='balanced_subsample', max_samples=0.2)
+
+        # Fit to the training data
+        clf.fit(x_train, y_train)
+
+        # Save the fit model
+        self.random_forest_classifier = clf
+
+        # Get the training data predictions
+        y_pred = clf.predict(x_train)
+        y_pred = pd.DataFrame(y_pred, columns=task_cols)
+        pred_df = deepcopy(train_data)
+        n_cols = len(pred_df.columns)
+        for t in task_cols:
+            pred_df.insert(loc=n_cols, column=f'{t}_prediction', value=y_pred[t].values)
+            n_cols += 1
+
+        return pred_df
+
+    def fit(self, train_data: pd.DataFrame, tasks: List[str], keep_training_data: bool = True,
+            features: Optional[List[str]] = None) -> pd.DataFrame:
         """Uses the provided data to train the model to perform the specified classification task.
 
         Arguments:
@@ -105,6 +174,8 @@ class ClassificationModel:
             'hate_speech_detection', 'target_or_general', 'aggression_detection'.
         keep_training_data
             A boolean that indicates whether the training data should be stored on the model.
+        features
+            The list of features to be used in the classification task.
 
 
         Returns:
@@ -116,11 +187,23 @@ class ClassificationModel:
 
         assert self.model_type is not None, 'The model_type must be specified in order to train a model.'
 
+        # Save task list
+        self.tasks = tasks
+
         # Fit and predict baseline model
         if self.model_type == 'baseline':
             if keep_training_data:
                 self.train_data = train_data
             pred_df = self._fit_baseline_model(train_data, tasks)
+
+        # Fit and predict random forest classifier
+        if self.model_type == 'random_forest':
+            assert features is not None, \
+                'The feature list must be provided in order to tran a Random Forest classification model.'
+            self.features = features
+            if keep_training_data:
+                self.train_data = train_data
+            pred_df = self._fit_random_forest_model(train_data, features, tasks)
 
         # TODO: Add other sections below that reference helper methods to do any necessary processing and fit_predict
         #  for other classifier models as they are added
@@ -147,7 +230,7 @@ class ClassificationModel:
         assert self.fitted, 'You must train a model before calling the predict method.'
 
         # Initialise prediction dataframe
-        pred_df = data
+        pred_df = deepcopy(data)
 
         # Predictions for baseline model
         if self.model_type == 'baseline':
@@ -157,6 +240,21 @@ class ClassificationModel:
             for k in self.most_frequent_category:
                 pred = self.most_frequent_category[k]
                 pred_df.insert(loc=n_cols, column=f'{k}_prediction', value=pred)
+
+        if self.model_type == 'random_forest':
+
+            # Clean the data
+            x_data = data[self.features]
+            x_data = x_data.fillna(0)
+
+            # Get the predictions
+            task_cols = [self.target_map[t] for t in self.tasks]
+            y_pred = self.random_forest_classifier.predict(x_data)
+            y_pred = pd.DataFrame(y_pred, columns=task_cols)
+            n_cols = len(pred_df.columns)
+            for t in task_cols:
+                pred_df.insert(loc=n_cols, column=f'{t}_prediction', value=y_pred[t].values)
+                n_cols += 1
 
         # TODO: add sections below that perform prediction for other classifier models as they are added
 
@@ -180,10 +278,14 @@ if __name__ == '__main__':
     val_df = myFE.transform(myDP.processed_data['validation'])
 
     # Instantiate the model
-    myClassifier = ClassificationModel('baseline')
+    myClassifier = ClassificationModel('random_forest')
 
     # Train the model
-    train_pred = myClassifier.fit(train_df, tasks=['hate_speech_detection'], keep_training_data=False)
+    features = ['percent_capitals', '!_count', '?_count', '$_count', '*_count', 'negative', 'positive', 'anger',
+                'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust']
+    train_pred = myClassifier.fit(train_df,
+                                  tasks=['hate_speech_detection', 'target_or_general', 'aggression_detection'],
+                                  keep_training_data=False, features=features)
 
     # Run the model on the validation data
     val_pred = myClassifier.predict(val_df)
@@ -191,3 +293,7 @@ if __name__ == '__main__':
     # View a sample of the results
     train_df.head()
     val_df.head()
+
+
+
+
