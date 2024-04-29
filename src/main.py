6addea11413747e174c165cb2fff60db8a860584
Ben C <bencote2000@gmail.com>
2024-04-29 11:58:33 -0700

    Updated config to include evaluation input paths for gold and pred files, updated in main

--- src/main.py
+++ src/main.py
@@ -28,7 +28,7 @@ def load_config(config_path):
         return None
 
 
-def make_eval_files(df, language):
+def make_eval_files(df, language, goldpath, predpath):
     """
     Transform dataframes into desired formatted .tsv files for evaluation script. Includes the tweet ID, cleaned text,
     and either the gold standard data on HS, TR, and AG, or their predicted values from the model.
@@ -52,8 +52,8 @@ def make_eval_files(df, language):
     pred_a_df = df[["HS_prediction"]].copy()
 
     # establish file paths, save dataframes as .tsv files
-    goldpath = "".join(["outputs/D3_test/ref/", lang, ".tsv"])
-    predpath_a = "".join(["outputs/D3_test/res/", lang, "_a.tsv"])
+    goldpath = "".join([goldpath, lang, ".tsv"])
+    predpath_a = "".join([predpath, lang, "_a.tsv"])
     gold_df.to_csv(goldpath, sep="\t") 
     pred_a_df.to_csv(predpath_a, sep="\t")
 
@@ -64,7 +64,7 @@ def make_eval_files(df, language):
 
     pred_b_df = pred_b_df.rename(columns={"HS_prediction": "HS", "TR_prediction": "TR", "AG_prediction": "AG"})
     # establish file path, save dataframe as .tsv files
-    predpath_b = "".join(["outputs/D3_test/res/", lang, "_b.tsv"])
+    predpath_b = "".join([predpath, lang, "_b.tsv"])
     pred_b_df.to_csv(predpath_b, sep="\t")
 
 
@@ -122,7 +122,10 @@ def main(config):
     val_pred = myClassifier.predict(val_df)
 
     # create evaluation files based on val_pred
-    make_eval_files(val_pred, input_tsv_files['language'])
+    make_eval_files(val_pred, 
+                    input_tsv_files['language'], 
+                    config['evaluation']['goldpath'],
+                    config['evaluation']['predpath'])
 
     # Instantiate the evaluator and run it
     myEvaluator = Evaluator(config['evaluation']['output_directory'], config['evaluation']['output_directory'],
