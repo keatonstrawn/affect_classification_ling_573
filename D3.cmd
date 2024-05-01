8ad1cb34ae2ca7546b46813ebaea0644b9602cab
bpcot23 <146127773+bpcot23@users.noreply.github.com>
2024-05-01 11:38:22 -0700

    Update and rename D2.cmd to D3.cmd

--- D3.cmd
+++ D3.cmd
@@ -1,7 +1,7 @@
 executable = scripts/run_main.sh
 getenv = True
-error = condor_logs/D2.error
-log = condor_logs/D2.log
+error = condor_logs/D3.error
+log = condor_logs/D3.log
 notification = always
 transfer_executable = false
 request_memory = 8*1024
