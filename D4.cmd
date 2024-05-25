c5782bbf7abf9bce3a68715119e3f85f248cc5b1
Ben C <bencote2000@gmail.com>
2024-05-25 10:24:37 -0700

    Add spanish sentiment checker

--- D4.cmd
+++ D4.cmd
@@ -1,7 +1,7 @@
 executable = scripts/run_main.sh
 getenv = True
-error = condor_logs/D3.error
-log = condor_logs/D3.log
+error = condor_logs/D4.error
+log = condor_logs/D4.log
 notification = always
 transfer_executable = false
 request_memory = 8*1024
